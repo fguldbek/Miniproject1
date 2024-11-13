773be06ef77e876ed64d3b3fcf61e9e3674d777f
Mathias2812 <mathias.sparre2812@gmail.com>
2024-11-13 12:29:57 +0100

    clothing og room12

--- Core/Order.cs
+++ Core/Order.cs
@@ -12,12 +12,12 @@ namespace Core
         public string Description { get; set; } = "";
         
         public string? ImageUrl { get; set; }
-        
-        public string? Category { get; set; }
-        
-        public string Room { get; set; }
 
-        public bool Status { get; set; } 
+        public string? Category { get; set; } = "Clothing";
+
+        public string Room { get; set; } = "Room 12";
+        
+        public bool Status { get; set; } = true; 
 
     }
 }
