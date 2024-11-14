20dfbe3b10cd0a3eabaa21fa503ff018115b50ef
Frederik <eaa23fogc@students.eaaa.dk>
2024-11-14 11:35:45 +0100

    Tilføjet login service.

--- Core/User.cs
+++ Core/User.cs
@@ -10,22 +10,9 @@ using System.ComponentModel.DataAnnotations;
 namespace Core;
 public class User
 {
-    
     public int UserId { get; set; }
     
-    public static int BuyerId { get; set; }
-    
-    [Required]
-    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
-    public string Name { get; set; }
-    
-    [Required]
-    [StringLength(50, MinimumLength = 2, ErrorMessage = "Adress must be between 2 and 50 characters.")]
-    public string Adress { get; set; }
-    
-    [Required]
-    [StringLength(50, MinimumLength = 2, ErrorMessage = "Email must be between 2 and 50 characters.")]
-    public string Email { get; set; }
+    public int BuyerId { get; set; }
     
     [Required]
     [StringLength(50, MinimumLength = 5, ErrorMessage = "Username must be between 2 and 50 characters.")]
@@ -35,6 +22,5 @@ public class User
     [StringLength(50, MinimumLength = 7, ErrorMessage = "Password must be between 2 and 50 characters.")]
     public string Password { get; set; }
     
-    public List<string>? Order { get; set;}
-
+    public string Role { get; set; }
 }
